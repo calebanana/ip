aabedb56c46049019081149489ae1c005a19a064
Caleb Goh <calebgoh05@gmail.com>
2025-08-20 10:22:19 +0800

    Rename project to Kleb

---
+++ src/main/java/Kleb.java
@@ -0,0 +1,15 @@
+public class Kleb {
+    private static final String LOGO = """
+            
+            ██╗  ██╗██╗     ███████╗██████╗\s
+            ██║ ██╔╝██║     ██╔════╝██╔══██╗
+            █████╔╝ ██║     █████╗  ██████╔╝
+            ██╔═██╗ ██║     ██╔══╝  ██╔══██╗
+            ██║  ██╗███████╗███████╗██████╔╝
+            ╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝\s
+                                           \s""";
+
+    public static void main(String[] args) {
+        System.out.println("Hello from\n" + LOGO);
+    }
+}
