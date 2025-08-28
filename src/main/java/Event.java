1350cd8c65154bea26534268730c3e6add2a0e26
Caleb Goh <calebgoh05@gmail.com>
2025-08-28 10:45:09 +0800

    Change data type for datetime variables

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,26 +1,38 @@
-public class Event extends Task {
-    private final String from;
-    private final String to;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String from, String to) {
+public class Event extends Task {
+    private final LocalDateTime from;
+    private final LocalDateTime to;
+
+    public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
-    public Event(String description, boolean isDone, String from, String to) {
+    public Event(String description, boolean isDone, LocalDateTime from, LocalDateTime to) {
         super(description, isDone);
         this.from = from;
         this.to = to;
     }
 
+    private String dateToString(LocalDateTime date, boolean save){
+        if (save) {
+            return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+        }
+        return date.format(DateTimeFormatter.ofPattern("MMM dd yyyy HHmm"));
+    }
+
     @Override
     public String toSaveString() {
-        return String.format("E | %s | %s | %s", super.toSaveString(), this.from, this.to);
+        return String.format("E | %s | %s | %s", super.toSaveString(), dateToString(this.from,
+                true), dateToString(this.to, true));
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), dateToString(this.from,
+                false), dateToString(this.to, false));
     }
 }
