9396bcf798f8c1f80313e9b9a147b840c048f3d2
Caleb Goh <calebgoh05@gmail.com>
2025-08-28 10:46:12 +0800

    Modify program to support datetime variables

---
+++ src/main/java/InvalidDateTimeException.java
@@ -0,0 +1,12 @@
+public class InvalidDateTimeException extends Exception {
+    public InvalidDateTimeException() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Uh-oh! The date time format is incorrect. Format:
+                \tyyyy-MM-dd HHmm""";
+    }
+}
